e9f20f610baf9d34ce2cb9297ca592fb8e2d4f1d
WEIRANeryi <741758994@qq.com>
2019-04-16 19:51:52 +0800

    更改个人信息页面部分

--- study/src/views/change/index.vue
+++ study/src/views/change/index.vue
@@ -1,15 +1,58 @@
 <template>
   <div class="container">
-    我是修改个人信息页面
+    <myHeader></myHeader>
+    <div class="content w960">
+      <div class="content-item user-phone">
+        <span>手机号</span>
+        <Input placeholder="你的手机号" v-model="formData.phone" clearable></Input>
+      </div>
+      <div class="content-item user-name">
+        <span>你的昵称</span>
+        <Input placeholder="请输入内容" v-model="formData.nickname" clearable></Input>
+      </div>
+      <Input placeholder="请输入内容" v-model="formData.password" show-password clearable></Input>
+
+    </div>
   </div>
 </template>
 
 <script>
+  import myHeader from '@/components/header'
+  import upload from '@/components/sw-upload'
+  import {Input} from 'element-ui'
   export default {
-    name: "index"
+    name: "index",
+    components: {
+      myHeader,
+      Input,
+      upload
+    },
+    data () {
+      return {
+        formData: {
+          phone: '',
+          password: '',
+          nickname: '',
+          sort: '',
+          sex: '',
+        }
+      }
+    },
+    methods: {
+
+    },
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .content {
+    margin-top: 30px;
+    .content-item {
+      display: flex;
+      justify-content: space-between;
+      Input {
+        max-width: 300px;
+      }
+    }
+  }
 </style>
